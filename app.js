51eb287d6a9268d4a944770c1bdb74dc6d044136
robsouthgate4 <robsouthgate4@gmail.com>
2015-04-21 15:46:50 +0100

    Added firebase code

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 
-var app = angular.module('quizApp', ['ngAnimate']);
+var app = angular.module('quizApp', ['ngAnimate', 'firebase']);
 
 app.controller('MainCtrl', [function(){
 	var self = this;
 }]);
 
-app.factory('quizDataOne', ['$http', function($http){
-	return {
-		getQuestions: function() {
-			data = $http.get('data.json');
-			return data;
-		}
-	}
-}]);
+app.directive('quizSlider', ['$firebaseObject', function($firebaseObject){
 
-app.factory('quizDataTwo', ['$http', function($http){
-    return {
-        getQuestions: function() {
-            data = $http.get('dataTwo.json');
-            return data;
-        }
-    }
-}]);
-
-app.directive('quizSlider', [function(){
     var uniqueId = 1;
+    var resultsRef = new Firebase("https://quiz-app.firebaseio.com/results");
+
     return {
         templateUrl: 'quiz.html',
         restrict: 'AEC',
@@ -38,6 +23,9 @@ app.directive('quizSlider', [function(){
             $scope.nextHidden = true;
             $scope.points = null;
             $scope.feedbackArray = [];
+            $scope.answerObj = {};
+            $scope.resultArray = [];
+            $scope.userObj = {};
             $scope.count = 0;
             $scope.total = 0;
             $scope.feedback = null;
@@ -55,14 +43,20 @@ app.directive('quizSlider', [function(){
                 }).error(function(){
                     alert("Error getting data");
                 });           
-            
 
             $scope.startQuiz = function(){
                 $scope.start = true;
             };
+
             $scope.itemClicked = function($index){
+
                 $scope.selectedIndex = $index;
                 $scope.nextHidden = false;
+
+                $scope.answerObj = {
+                    "question number" : $scope.count + 1,
+                    "answer" : $index + 1
+                }
 
                 for ( var i = 0; i < $scope.questions.length; i++) {
                     $scope.questions[i].selected = false;
@@ -70,23 +64,27 @@ app.directive('quizSlider', [function(){
                 }
 
                 $scope.questions[$index].selected = true;
+
             };
+
             $scope.buildFeedback = function(){
+
                 $scope.count ++;
                 $scope.selectedIndex = null;
                 $scope.feedbackArray.push($scope.points);
                 if ($scope.count == $scope.questions.length) {
                     $scope.setFeedback();
                 }
+                $scope.resultArray.push($scope.answerObj);
             };
+
             $scope.setFeedback = function() {
+
                 var endPoints = $scope.feedbackArray;
                 $scope.total = 0;
-
                 for ( var i = 0; i < endPoints.length; i++ ) {
                     $scope.total += endPoints[i];
                 }
-
                 if ( $scope.total >= 4 && $scope.total <= 8 ) {
                     $scope.feedback = 'first';
                 } else if ($scope.total > 8 && $scope.total <= 8) {
@@ -94,16 +92,60 @@ app.directive('quizSlider', [function(){
                 } else {
                     $scope.feedback = 'third';
                 }
+
+            };            
+
+            $scope.sendResults = function() {                
+
+                $scope.userObj = {
+                    "userdetails" : {
+                        "name" : $scope.user.name,
+                        "email" : $scope.user.email,
+                        "phone": $scope.user.phone
+                    },
+                    "answers" : {
+                        "items" : $scope.resultArray 
+                    }
+                }
+
+                resultsRef.push($scope.userObj);
+
             };
+
             $scope.restartQuiz = function() {
+
                 $('#carousel-example-generic'+ $scope.uniqueId +'').carousel(0);
                 $('#carousel-example-generic'+ $scope.uniqueId +'').on('slid.bs.carousel', function(){
                     $scope.feedback = null;
                 });
                 $scope.count = 0;
                 $scope.total = 0;
-                $scope.feedbackArray.length = 0;
-            }
+                $scope.feedbackArray = [];
+                $scope.user.name = "";
+                $scope.user.email = "";
+                $scope.user.phone = "";
+                $scope.resultArray = [];
+                $scope.userObj = {};
+
+            };
+        }
+    }
+}]);
+
+app.factory('quizDataOne', ['$http', function($http){
+    return {
+        getQuestions: function() {
+            data = $http.get('data.json');
+            return data;
+        }
+    }
+}]);
+
+app.factory('quizDataTwo', ['$http', function($http){
+    return {
+        getQuestions: function() {
+            data = $http.get('dataTwo.json');
+            return data;
         }
     }
 }]);
